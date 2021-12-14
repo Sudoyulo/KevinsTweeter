d2165d834ba39e2c38346a97ede7dcfdf8358736
Kevin Tan Yulo <yulokevin@gmail.com>
2021-12-14 00:42:50 -0800

    key pressed console.log works! It was an id not a class so I changed . to # and it started working. testing with keyup for now. Going to try key down, blur, and other commands to find the right one

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,7 @@ console.log("Document has loaded");
 $(document).ready(function() {
   console.log("Document is ready");
   
-  $( ".tweet-text" ).keyup(function() {
+  $("#tweet-text").keyup(function() {
     console.log("Key is pressed");
   });
 });
