5e3b1038d439203d20be58f747f6b80bca6e0f3d
Kevin Tan Yulo <yulokevin@gmail.com>
2021-12-14 09:58:18 -0800

    created new-tweet.css and moved css around the tweet text into the css folder to keep things separate.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,18 @@ console.log("Document has loaded");
 
 $(document).ready(function() {
   console.log("Document is ready");
+  let numbersLeft = 0;
   
-  $("#tweet-text").keyup(function() {
-    console.log("Key is pressed");
+  $("#tweet-text").keypress(function(input) {
+    numbersLeft = 139 - input.target.value.length;
+    console.log(numbersLeft)
+
+    if (numbersLeft > 140) {
+      console.log("too much");
+    }
+    if (numbersLeft < 1) {
+      console.log("not enough");
+    }
+
   });
 });
