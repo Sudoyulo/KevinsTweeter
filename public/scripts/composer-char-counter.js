dd298f2647e8b3c471fcb0bcc9d331c5665a1960
Kevin Tan Yulo <yulokevin@gmail.com>
2021-12-14 00:35:07 -0800

    scripts not console.logging when key pressed. change input to textarea instead of input. looks bad. Will revert and try again tomorrow

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,4 +4,7 @@ console.log("Document has loaded");
 $(document).ready(function() {
   console.log("Document is ready");
   
+  $( ".tweet-text" ).keyup(function() {
+    console.log("Key is pressed");
+  });
 });
